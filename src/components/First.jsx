2201cc1e232cc9828137bc33ad01a9ccd191420b
Vedpratap11 <rajawat.ved1117@gmail.com>
2024-12-26 11:06:40 +0530

    first commit

---
+++ src/components/First.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function First() {
+  return (
+    <>
+    
+    <div id='wrap'>
+      <div className='left'>
+        
+      </div>
+      <div className='right'></div>
+    </div>
+   
+    </>
+  )
+}
+
+export default First
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "../components/Header.jsx"
+import First from "../components/First.jsx"
+function Home() {
+  return (
+    <>
+    <Header/>
+    <First/>
+    </>
+   )
+}
+
+export default Home
